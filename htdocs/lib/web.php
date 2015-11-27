c46d4801ba0d14d03ebe4c2cfad8d3b4f491f610
Marc Català <mcatala@itteria.cat>
2015-11-27 13:39:54 +0100

    Hide login form from main page

--- htdocs/lib/web.php
+++ htdocs/lib/web.php
@@ -170,7 +170,7 @@ function smarty($javascript = array(), $headers = array(), $pagestrings = array(
     }
     // Now that password element can set headdata we need to call the login form before smarty_core()
     $isloginblockvisible = !$USER->is_logged_in() && !get_config('siteclosedforupgrade')
-        && get_config('showloginsideblock');
+        && get_config('showloginsideblock') && ($_SERVER['SCRIPT_FILENAME'] === get_config('docroot') . 'about.php');
     if ($isloginblockvisible) {
         $authgenerateloginform = auth_generate_login_form();
     }
