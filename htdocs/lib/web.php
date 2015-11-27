7cac58716f6ba18f6d665884940e68166f4c8644
Marc Català <mcatala@itteria.cat>
2015-11-27 13:39:54 +0100

    Hide login form from main page

--- htdocs/lib/web.php
+++ htdocs/lib/web.php
@@ -802,7 +802,7 @@ EOF;
         }
 
         $isloginblockvisible = !$USER->is_logged_in() && !get_config('siteclosedforupgrade')
-                && get_config('showloginsideblock');
+                && get_config('showloginsideblock') && ($_SERVER['SCRIPT_FILENAME'] === get_config('docroot') . 'about.php');
         if ($isloginblockvisible) {
             $sideblocks[] = array(
                 'name'   => 'login',
